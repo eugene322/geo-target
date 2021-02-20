b50d069ccefa9f27f967b073862e5c7572c888fd
Eugene <vint.evg@gmail.com>
2021-02-21 01:17:06 +0600

    Fix env

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -34,7 +34,6 @@ case "$PROCESS" in
     uvicorn core.main:app --reload --host 0.0.0.0 --port 8000
     ;;
 "FASTAPI")
-    wait_for "${DB_HOST}" "${DB_PORT}"
     uvicorn core.main:app --host 0.0.0.0 --port 8000 \
     --proxy-headers --workers 8 --limit-max-requests 2048
     # workers = (2*CPU)+1
